3e82990f7943067493db32fac3749d567d4d1773
Abdurrahman Abu-Hijleh <aabuhijleh@asaltech.com>
2021-07-26 21:24:30 +0300

    chore: override getDisplayMedia

--- preload.js
+++ preload.js
@@ -1,12 +1,25 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
-window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
 
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
-  }
-})
+const { desktopCapturer, contextBridge } = require("electron");
+const { readFileSync } = require("fs");
+const { join } = require("path");
+
+// inject renderer.js into the web page
+window.addEventListener("DOMContentLoaded", () => {
+  const rendererScript = document.createElement("script");
+  rendererScript.text = readFileSync(join(__dirname, "renderer.js"), "utf8");
+  document.body.appendChild(rendererScript);
+});
+
+contextBridge.exposeInMainWorld("myCustomGetDisplayMedia", async () => {
+  const sources = await desktopCapturer.getSources({
+    types: ["window", "screen"],
+  });
+
+  // you should create some kind of UI to prompt the user
+  // to select the correct source like Google Chrome does
+  const selectedSource = sources[0]; // this is just for testing purposes
+
+  return selectedSource;
+});
--- renderer.js
+++ renderer.js
@@ -4,3 +4,25 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+// override getDisplayMedia
+navigator.mediaDevices.getDisplayMedia = async () => {
+  const selectedSource = await globalThis.myCustomGetDisplayMedia();
+
+  // create MediaStream
+  const stream = await navigator.mediaDevices.getUserMedia({
+    audio: false,
+    video: {
+      mandatory: {
+        chromeMediaSource: "desktop",
+        chromeMediaSourceId: selectedSource.id,
+        minWidth: 1280,
+        maxWidth: 1280,
+        minHeight: 720,
+        maxHeight: 720,
+      },
+    },
+  });
+
+  return stream;
+};
